4e0a1c86d9f1cd23248cf0773d5d9f549fbfffbe
NOHHYUNGBIN <shgudqlsz@naver.com>
2024-11-16 23:35:41 +0900

    annotation 테스트

--- src/main/java/com/fastcampus/ch3/di3/AppConfig.java
+++ src/main/java/com/fastcampus/ch3/di3/AppConfig.java
@@ -1,10 +1,12 @@
 package com.fastcampus.ch3.di3;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
+@ComponentScan
 public class AppConfig {
     @Bean
     Car car() {
--- src/main/java/com/fastcampus/ch3/di3/Main.java
+++ src/main/java/com/fastcampus/ch3/di3/Main.java
@@ -1,23 +1,47 @@
 package com.fastcampus.ch3.di3;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
 class Car {
-    Engine engine;
+    @Autowired
+    Engine[] engine;
+
+    @Autowired
     Door door;
+
+    @Autowired
+    public Car(Door door, Engine[] engine) {
+        this.door = door;
+        this.engine = engine;
+    }
+
+    public Car() {
+    }
 
     @Override
     public String toString() {
         return "Car{" +
                 "door=" + door +
-                ", engine=" + engine +
+                ", engine=" + Arrays.toString(engine) +
                 '}';
     }
 }
+@Component
 class Engine {}
+
+@Component
+class SuperEngine extends Engine {}
+
+@Component
+class TurboEngine extends Engine {}
+
+@Component
 class Door {}
 public class Main {
     public static void main(String[] args) {
@@ -25,25 +49,6 @@ public class Main {
         ApplicationContext ac = new AnnotationConfigApplicationContext(com.fastcampus.ch3.di3.AppConfig.class);
         // Car car = (Car) ac.getBean("car"); // byName 객체(빈) 조회
         Car car = ac.getBean("car", Car.class);
-
-        Engine engine = ac.getBean(Engine.class);
-        Engine engine2 = ac.getBean(Engine.class);
-        Engine engine3 = ac.getBean(Engine.class);
         System.out.println("car = " + car);
-        System.out.println("engine = " + engine);
-        System.out.println("engine2 = " + engine2);
-        System.out.println("engine3 = " + engine3);
-
-        //빈이 몇개 들어있는지.
-        System.out.println("ac.getBeanDefinitionCount() = " + ac.getBeanDefinitionCount());
-        //빈의 이름들 배열로받음.
-        System.out.println("ac.getBeanDefinitionNames() = " + Arrays.toString(ac.getBeanDefinitionNames()));
-        //엔진이란 빈이있는지 확인
-        System.out.println("ac.containsBeanDefinition(\"engine\") = " + ac.containsBeanDefinition("engine"));
-        //car의 객체가 싱글톤인지 확인
-        System.out.println("ac.isSingleton(\"car\") = " + ac.isSingleton("car"));
-        //engine의 객체가 프로토타입인지 확인
-        System.out.println("ac.isPrototype('engine') = " + ac.isPrototype("engine"));
-
     }
 }
